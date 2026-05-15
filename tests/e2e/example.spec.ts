9175fdcd8f35b44cbe7c8e7390e658981baacd7c
tadjh <hello@tadjh.com>
2026-05-15 12:59:53 -0700

    Remove quotes

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -2,6 +2,6 @@ import { expect, test } from "@playwright/test"
 
 test("home page loads", async ({ page }) => {
   await page.goto("/")
-  await expect(page).toHaveTitle(/"The Pragmatic Papers"/)
+  await expect(page).toHaveTitle(/The Pragmatic Papers/)
   await expect(page.getByRole("heading", { name: "Home", level: 1 })).toBeVisible()
 })

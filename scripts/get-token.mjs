ba6dd20b729fb702ec7a360e7e72f333272fb73f
espdesign <evanpendergraft@gmail.com>
2026-05-16 16:11:24 -0400

    remove scripts

--- scripts/get-token.mjs
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import fs from 'node:fs';
-
-const env = fs.readFileSync('.env', 'utf8');
-const match = env.match(/^GH_FONT_READ=(.*)$/m);
-if (match) {
-  // eslint-disable-next-line no-console
-  console.log(match[1].trim());
-}

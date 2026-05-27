d7e6bb0cbb03b6d93aa7691c49050e03ce324fcd
tadjh <hello@tadjh.com>
2026-05-27 14:46:07 -0700

    Update src/instrumentation-client.ts
    Co-authored-by: Ted Timbrell <ted@timbrell.dev>

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -13,7 +13,6 @@ Sentry.init({
 
   enableLogs: true,
 
-  // Enable sending user PII (Personally Identifiable Information)
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
 })

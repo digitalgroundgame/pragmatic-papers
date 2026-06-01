987880d7081ea1cc0fb0fb06dd1d7afc66d74c39
tadjh <hello@tadjh.com>
2026-06-01 11:14:56 -0700

    Apply suggestion from @tadjh

--- src/heros/ArticleHero/index.tsx
+++ src/heros/ArticleHero/index.tsx
@@ -30,7 +30,7 @@ export const ArticleHero: React.FC<ArticleHeroProps> = ({ article }) => {
       )}
       <h1 className="mt-6">{title}</h1>
       <div className="flex flex-col gap-2 md:flex-row md:items-center md:gap-8">
-        <div className="dark:text-brand-high-contrast text-brand flex flex-1 gap-2 font-serif font-bold underline-offset-4">
+        <div className="dark:text-brand-high-contrast text-brand flex flex-1 flex-wrap gap-2 font-serif font-bold underline-offset-4">
           {populatedAuthors &&
             populatedAuthors.map(({ id, slug, name }, index) => (
               <React.Fragment key={id}>

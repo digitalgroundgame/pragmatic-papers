92452eade9662eca7a09d980c2c7a2dd11f6c4b2
tadjh <hello@tadjh.com>
2026-06-01 21:44:18 -0700

    Remove ShareButtons

--- src/app/(frontend)/volumes/[slug]/page.tsx
+++ src/app/(frontend)/volumes/[slug]/page.tsx
@@ -5,12 +5,10 @@ import { HoverPrefetchLink } from "@/components/Link/HoverPrefetchLink"
 import { LivePreviewListener } from "@/components/LivePreviewListener"
 import { PayloadRedirects } from "@/components/PayloadRedirects"
 import RichText from "@/components/RichText"
-import { ShareButtons } from "@/components/ShareButtons"
 import { Separator } from "@/components/ui/separator"
 import type { Article } from "@/payload-types"
 import { formatDateTime } from "@/utilities/formatDateTime"
 import { generateMeta } from "@/utilities/generateMeta"
-import { getServerSideURL } from "@/utilities/getURL"
 import { queryVolumeBySlug } from "@/utilities/queries"
 import { buildBreadcrumbJsonLd, buildVolumeJsonLd } from "@/utilities/structuredData"
 import { toRoman } from "@/utilities/toRoman"
@@ -105,7 +103,6 @@ export default async function VolumePage({
             <time dateTime={publishedAt}>{formatDateTime(publishedAt)}</time>
           </HoverPrefetchLink>
         )}
-        <ShareButtons url={`${getServerSideURL()}${url}`} title={volumeTitle} className="ml-auto" />
       </div>
       {editorsNote && <RichText className="drop-cap" enableGutter={false} data={editorsNote} />}
       <Separator className="my-6" />
--- src/heros/ArticleHero/index.tsx
+++ src/heros/ArticleHero/index.tsx
@@ -1,13 +1,11 @@
 import React from "react"
 
-import { ShareButtons } from "@/components/ShareButtons"
 import { HoverPrefetchLink } from "@/components/Link/HoverPrefetchLink"
 import { Media } from "@/components/Media"
 import { NarrationPlayer } from "@/components/NarrationPlayer"
 import { Separator } from "@/components/ui/separator"
 import type { Article } from "@/payload-types"
 import { formatDateTime } from "@/utilities/formatDateTime"
-import { getServerSideURL } from "@/utilities/getURL"
 import { getSeparator } from "@/utilities/getSeparator"
 
 interface ArticleHeroProps {
@@ -52,11 +50,6 @@ export const ArticleHero: React.FC<ArticleHeroProps> = ({ article }) => {
             <NarrationPlayer narration={narration} populatedNarrator={populatedNarrator} />
           </div>
         )}
-        <ShareButtons
-          url={`${getServerSideURL()}/articles/${article.slug}`}
-          title={article.title}
-          className="shrink-0 self-end md:self-auto"
-        />
       </div>
       <Separator />
     </div>
